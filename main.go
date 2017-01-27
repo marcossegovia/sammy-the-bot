96c2bef1f3c0a44cfb60b1bf8c1e17d0b48aa69e
Marcos <velozmarkdrea@gmail.com>
2017-01-27 20:44:20 +0100

    Extracting method and unsetting global vars to pass local vars as parameters

--- main.go
+++ main.go
@@ -9,40 +9,22 @@ import (
 	"bytes"
 )
 
-var cfg, brain *viper.Viper
-
-func init() {
-	cfg = viper.New()
-	cfg.AddConfigPath(".")
-	cfg.SetConfigName("sammy_config")
-	err := cfg.ReadInConfig()
-	if err != nil {
-		log.Printf("could not read config file: %v", err)
-	}
-	brain = viper.New()
-	brain.AddConfigPath(".")
-	brain.SetConfigName("sammy_brain")
-	err = brain.ReadInConfig()
-	if err != nil {
-		log.Printf("could not read config file: %v", err)
-	}
-}
-
 func main() {
-	token := cfg.GetString("configuration.token")
-	var commands = map[string]string{}
-	availableCmds := brain.GetStringSlice("commands.commands")
-	for _, cmd := range availableCmds {
-		index := cmd[1:]
-		commands[index] = cmd
+	cfg, err := read("sammy_config")
+	if err != nil {
+		log.Printf("could not read config file: %v", err)
 	}
+	brain, err := read("sammy_brain")
+	if err != nil {
+		log.Printf("could not read config file: %v", err)
+	}
+	token := cfg.GetString("configuration.token")
+	commands := setCommands(brain)
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Printf("could not initialize bot: %v", err)
 	}
 	log.Printf("Authorized on account %v", bot.Self.UserName)
-
-	//bot.Debug = true
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -72,6 +54,27 @@ func main() {
 		log.Printf("I'm responding: %v", msg.Text)
 		bot.Send(msg)
 	}
+}
+
+func read(path string) (*viper.Viper, error) {
+	f := viper.New()
+	f.AddConfigPath(".")
+	f.SetConfigName(path)
+	err := f.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+func setCommands(brain *viper.Viper) map[string]string {
+	var commands = map[string]string{}
+	availableCmds := brain.GetStringSlice("commands.commands")
+	for _, cmd := range availableCmds {
+		index := cmd[1:]
+		commands[index] = cmd
+	}
+	return commands
 }
 
 func salute(salutations []string) (string) {
